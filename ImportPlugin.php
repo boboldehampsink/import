6c4a4d7cd74bc9cc611a9718645ecbca8c06acc0
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-07-18 10:39:16 +0200

    Added some Unit Testing for my own use

--- ImportPlugin.php
+++ ImportPlugin.php
@@ -49,6 +49,17 @@ class ImportPlugin extends BasePlugin
         );
     }
     
+    // Register a Unit Test for my own use
+    function registerUnitTest() {
+    
+        // Import the test
+        Craft::import('plugins.import.tests.ImportTest');
+        
+        // Return the test
+        return new ImportTest();
+    
+    }
+    
     // Register ImportOperation hook
     function registerImportOperation(&$data, $handle)
     {
---
+++ tests/ImportTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Craft;
+
+class ImportTest extends \UnitTestCase 
+{
+    
+    function testHistoryShowLog() 
+    {
+        $log = craft()->import_history->showLog(1);
+        $this->assertTrue(is_array($log));
+        $this->assertTrue($log > 0);
+    }
+    
+    function testHistoryLog() 
+    {
+        $log = craft()->import_history->log($historyId = 1, $line = 0, $errors = array());
+        $this->assertSame($log, $errors);
+    }
+    
+    function testPrepForEntryModel()
+    {
+        $fields = array('title' => 'test');
+        $entry = craft()->import->prepForEntryModel($fields, new EntryModel());
+        $this->assertTrue($entry instanceOf EntryModel);
+    }
+    
+}
