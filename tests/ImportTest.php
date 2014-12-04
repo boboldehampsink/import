bf7e00d625ce889f116d9a113dc6ec01300999b2
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-12-04 08:45:25 +0100

    Updated Unit Test setup

--- tests/ImportTest.php
+++ tests/ImportTest.php
@@ -7,33 +7,9 @@ class ImportTest extends BaseTest
     public function setUp()
     {
     
-        // PHPUnit complains about not settings this
-        date_default_timezone_set('UTC');
-    
-        // Get dependencies
-        $dir = __DIR__;
-        $map = array(
-            '\\Craft\\ImportModel'           => '/../models/ImportModel.php',
-            '\\Craft\\Import_EntriesRecord'  => '/../records/Import_EntriesRecord.php',
-            '\\Craft\\Import_HistoryRecord'  => '/../records/Import_HistoryRecord.php',
-            '\\Craft\\Import_LogRecord'      => '/../records/Import_LogRecord.php',
-            '\\Craft\\Import_HistoryService' => '/../services/Import_HistoryService.php',
-            '\\Craft\\ImportService'         => '/../services/ImportService.php',
-            '\\Craft\\Import_EntryService'   => '/../services/Import_EntryService.php',
-            '\\Craft\\Import_ElementModel'     => '/../models/Import_ElementModel.php'
-        );
-
-        // Inject them
-        foreach($map as $classPath => $filePath) {
-            if(!class_exists($classPath, false)) {
-                require_once($dir . $filePath);
-            }
-        }
-    
-        // Construct them
-        $this->setComponent(craft(), 'import_history', new Import_HistoryService);
-        $this->setComponent(craft(), 'import', new ImportService);
-        $this->setComponent(craft(), 'import_entry', new Import_EntryService);
+        // Load plugins
+        $pluginsService = craft()->getComponent('plugins');
+        $pluginsService->loadPlugins();
     
     } 
     
